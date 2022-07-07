9a64e4e372f4bd708c4e46f6454adcd7f8e044ad
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-07-07 16:43:06 +0530

    Added start logic and event listeners

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const paperBtn = document.querySelector(".paper").lastElementChild;
 const scissorsBtn = document.querySelector(".scissors").lastElementChild;
 const gameContent = document.querySelector(".mid");
 
-const getThrows = () =>{
-    
-}
-
-const play = () => {
-  for (const i of [...Array(5).keys()]) {
-    const { playerThrow, computerThrow } = getThrows();
-    getResult(playerThrow, computerThrow);
-  }
+const toggleVisiblity = () => {
+    gameContent.classList.toggle("visible");
 };
 
-const toggleVisiblity = () => {
-    gameContent.classList.toggle("visible")
-}
+const onThrow = (move) => {
+    const playerScore = 0;
+    const computerScore = 0;
+
+    for (const i of [...Array(5).keys()]) {
+        const playerThrow = move;
+        const computerThrow = computerMove();
+    }
+};
 
 newGameBtn.onclick = toggleVisiblity;
+rockBtn.onclick = onThrowMove.bind("rock");
+paperBtn.onclick = onThrowMove.bind("paper");
+scissorBtn.onclick = onThrowMove.bind("scissor");
